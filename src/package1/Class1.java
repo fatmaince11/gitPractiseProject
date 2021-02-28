402ece019efe233197f85cb59707c6e8ff135946
fatmayilmaz <fatmaince11@gmail.com>
2021-02-28 15:34:50 -0500

    hello

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world from Fatma");
+		System.out.println("Hello world from Dundar");
 
 	System.out.println("Second commit");
 	}
